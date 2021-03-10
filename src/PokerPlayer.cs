0d2c44310ad4670424a109fe55116ab3ad914ad2
Tiago Baranita <tiago.baranita@gmail.com>
2021-03-10 15:19:14 +0100

    Back to the roots

--- src/PokerPlayer.cs
+++ src/PokerPlayer.cs
@@ -29,13 +29,18 @@ namespace Nancy.Simple
         {
             var player = gameState.Players.Single(p => p.Name == TeamName);
 
+            Console.Error.WriteLine("Round: " + gameState.Round 
+                                  + ", BetIndex: " + gameState.BetIndex 
+                                  + ", Dealer: " + gameState.Dealer);
+
             if (IsHeadsUp(gameState))
             {
                 Console.Error.WriteLine("Is heads up");
                 if (HoleCard.IsHigh(player.Cards[0], player.Cards[1])
                     || HoleCard.AllCardsAreGood(player.Cards, gameState.CommunityCards))
                 {
-                    return GetMinimumRaiseBetTimes(gameState, 3);
+                    // return GetMinimumRaiseBetTimes(gameState, 3);
+                    return player.Stack;
                 }
             }
             else
@@ -43,7 +48,8 @@ namespace Nancy.Simple
                 Console.Error.WriteLine("Is no heads up");
                 if (HoleCard.IsHighPair(player))
                 {
-                    return GetMinimumRaiseBetTimes(gameState, 1);
+                    // return GetMinimumRaiseBetTimes(gameState, 1);
+                    return player.Stack;
                 }
             }
 
