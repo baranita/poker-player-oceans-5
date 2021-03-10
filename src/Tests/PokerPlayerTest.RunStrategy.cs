4b1febb539034155c0e6573c4671aa1226c8ff88
Tiago Baranita <tiago.baranita@gmail.com>
2021-03-10 11:16:01 +0100

    Try to fix deploy

---
+++ src/Tests/PokerPlayerTest.RunStrategy.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace Nancy.Simple.Tests
+{
+    public class PokerPlayerTestRunStrategy
+    {
+        // [Test]
+        // public void RunStrategy_ReturnsParsed()
+        // {
+        //     var gameState = new GameState();
+        //
+        //     var actual = PokerPlayer.RunStrategy(gameState);
+        //
+        //     Assert.That(actual, Is.EqualTo(ExpectedState));
+        // }
+    }
+}
